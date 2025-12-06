fe0745fb4cb232c448ffcff174d7655c7fd7004c
Michael Ostermann <michaelostermann@me.com>
2025-12-06 15:05:12 +0100

    chore: migrations

--- packages/unplugin-array/src/index.ts
+++ packages/unplugin-array/src/index.ts
@@ -25,7 +25,7 @@ export default createUnplugin<Options>(({ debug, enforce, exclude, include } = {
                     debug: shouldDebug(id),
                     resolve({ importAlias, importName, importPath, propertyName }) {
                         if (importPath === "@monstermann/array" && importName === "Array")
-                            return `import { ${propertyName} as ${importAlias} } from "@monstermann/array/Array/${propertyName}.js"`
+                            return `import { ${propertyName} as ${importAlias} } from "@monstermann/array/Array/${propertyName}.mjs"`
                         return undefined
                     },
                 })
